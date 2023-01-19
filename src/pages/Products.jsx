455ffd42c64a5aa3d7ac0c3c9f3c05bbd97ba6de
Aliazbair <ali0995559052@gmail.com>
2023-01-19 14:00:53 +0300

    fix layout

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -16,48 +16,47 @@ export const Products = () => {
   return (
     <div className=''>
       <h2>Products page</h2>
-      <div className='grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 p-3'>
+      <div className='grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 p-3 '>
         {data.products.map(
           ({ title, category, id, count, rate, description, price, image }) => (
-            <div key={id} className='shadow shadow-gray-500 '>
+            <div key={id} className='shadow shadow-gray-500'>
               <img
                 src={image}
                 alt=''
                 className='w-[100%] h-52 object-cover rounded'
               />
               <div className='p-3'>
-                <div className='flex items-center justify-between'>
-                  <h2 className='text-xl font-semibold'>{title}</h2>
-                  <small className='text-sm bg-red-700 px-s1  rounded'>
-                    {category}
-                  </small>
+                <div className=''>
+                  <h2 className='text-lg md:text-xl font-semibold'>{title.slice(0,20)}</h2>
                 </div>
-                <p className='truncate'>{description}</p>
+                <p className='truncate text-sm text-slate-300'>{description}</p>
                 <div className='flex items-center justify-between py-3'>
-                  <h4>{rate}</h4>
-                  <h5>{count}</h5>
+                  <h4 className='text-red-400 font-bold'>{rate}</h4>
+                  <h5 className='text-sm font-bold text-orange-500'>{count}</h5>
                 </div>
                 <p>
                   <strong>${price}</strong>
                 </p>
 
-                <button
-                  onClick={() =>
-                    dispatch(
-                      addToCart({
-                        id,
-                        title,
-                        description,
-                        image,
-                        price,
-                        quantity,
-                      })
-                    )
-                  }
-                  className='bg-yellow-700 text-white px-2 py-1 rounded-sm mt-5'
-                >
-                  add to card
-                </button>
+                <div className=''>
+                  <button
+                    onClick={() =>
+                      dispatch(
+                        addToCart({
+                          id,
+                          title,
+                          description,
+                          image,
+                          price,
+                          quantity,
+                        })
+                      )
+                    }
+                    className='block w-full bg-yellow-700 text-white px-2 py-1 rounded-sm mt-5 '
+                  >
+                    add to card
+                  </button>
+                </div>
               </div>
             </div>
           )
